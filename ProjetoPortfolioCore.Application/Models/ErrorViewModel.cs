c69859c30a38815527a37a83ee44b04483e3f1c0
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2022-08-14 22:47:14 -0400

    Projeto Portfólio Arquitetura DDD Monolítica

---
+++ ProjetoPortfolioCore.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoPortfolioCore.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
